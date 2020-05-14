7577707d7a985c97f92fa8d4d7a7f951439052bf
U-TICOM-GEO\rhall <rhall@wrhall-nb.ticom-geo.com>
2020-05-14 16:15:22 -0500

    This is a change

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,8 @@
-openssl s_client -connect ${DTR_IP}:443 -showcerts \
+openssl s_client -connect 3.123.227.193:443 -showcerts \
     </dev/null 2>/dev/null | openssl x509 -outform PEM | sudo tee \
-    /usr/local/share/ca-certificates/${DTR_IP}.crt
+    /usr/local/share/ca-certificates/3.123.227.193.crt
 sudo update-ca-certificates
 
 /sbin/tini -- /usr/local/bin/jenkins.sh
+
+
